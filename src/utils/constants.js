268230fcc377cfc43515fb9f4147160c8a802a67
ANKIT TIWARY <techyjantadoyo121@gmail.com>
2026-03-16 09:17:45 +0530

    cors Url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 // export const MAIN_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8467126&lng=80.9460872&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/";
 export const LOGO_URL="https://drive.google.com/uc?export=view&id=1-7h9ANV7bAFZCkF9UnuObqDf5jz7RPTb";
-export const MENU_API ="https://corsproxy.io/https://www.zomato.com/webroutes/getPage?page_url=";
+// export const MENU_API ="https://corsproxy.io/https://www.zomato.com/webroutes/getPage?page_url=";
+export const MENU_API =
+"https://api.allorigins.win/raw?url=https://www.zomato.com/webroutes/getPage?page_url=";
